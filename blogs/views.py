58ad7f888347d1f83ff1c29c854e0fed093107c8
j4s1x <chessboy14@gmail.com>
2017-06-07 23:16:24 -0600

    last one for the night.  TODO lists updated see views.py

--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .models import Posts
 
 def index(request):
-    '''show some info''' 
+    '''show some info'''
     content = Posts.objects.order_by('date_added')
     context = {'content': content[:4]} #splice indicates that only four blog posts will be shown
     return render(request, 'blogs/index.html', context)
+
+#TODO maybe shorten the posts on the front page so they don't take up the entire page
+# by adding an ellipsis at the end.  Then have a link on the title so you can go read
+# the full article.  Also need an archive page that contains everything no matter what. 
