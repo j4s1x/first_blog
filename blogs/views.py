fb828c77895d0cd985c2129c33bd3cc9203d978f
j4s1x <chessboy14@gmail.com>
2017-06-07 23:07:13 -0600

    finally got gitignore to work by deleting cache and reinstalling.  Will push this to master.

--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Posts
 
 def index(request):
-    '''show some info''' #TODO edit so that only five posts appear on the front page
+    '''show some info''' 
     content = Posts.objects.order_by('date_added')
-    context = {'content': content[:4]}
+    context = {'content': content[:4]} #splice indicates that only four blog posts will be shown
     return render(request, 'blogs/index.html', context)
