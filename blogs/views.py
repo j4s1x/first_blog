6148557c1611dcadf8d73d9b557c312fd924ceea
j4s1x <chessboy14@gmail.com>
2017-06-07 22:32:34 -0600

    moved files around

--- blogs/views.py
+++ blogs/views.py
@@ -4,5 +4,5 @@ from .models import Posts
 def index(request):
     '''show some info''' #TODO edit so that only five posts appear on the front page
     content = Posts.objects.order_by('date_added')
-    context = {'content': content}
+    context = {'content': content[:4]}
     return render(request, 'blogs/index.html', context)
