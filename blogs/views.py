f61f421bd13f8020371e047909ce3842621c4d45
j4s1x <chessboy14@gmail.com>
2017-06-15 22:25:08 -0600

    Able to add comments on admin page.  Able to view comments on each post.  However cannot add due to not null constraint failed: blogs_comments.posts_id

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Posts
+from .forms import CommentForm
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 
@@ -16,10 +17,22 @@ def archives(request):
 
 def article(request, article_id):
     article = Posts.objects.get(id=article_id)
-    content = Posts.objects.order_by('-date_added')
-    context = {'article': article, 'content': content}
-    return render(request, 'blogs/article.html', context)
+    comments = article.comments_set.order_by('-date_added')
 
+    if request.method != 'POST':
+        #no data submitted; create blank form
+        form = CommentForm()
+    else:
+        #POST data submitted, process data
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.comments = comments
+            new_comment.save()
+            return HttpResponseRedirect(reverse('blogs:article', args=[article_id]))
+
+    context = {'article': article, 'comments': comments, 'form': form}
+    return render(request, 'blogs/article.html', context)
 
 #TODO maybe shorten the posts on the front page so they don't take up the entire page
 # by adding an ellipsis at the end.  Then have a link on the title so you can go read
