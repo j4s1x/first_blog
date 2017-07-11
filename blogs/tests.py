00d2f878aabfb66088f157f596a3ddb0461214a6
j4s1x <chessboy14@gmail.com>
2017-07-10 23:31:44 -0600

    first test added to tests.py

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Posts
+from selenium import webdriver
+import unittest
 
-# Create your tests here.
+class UserFirstVisit(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_user_index_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('The Blog Title', self.browser.title)
+        self.fail('Test Complete!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
