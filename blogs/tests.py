0b05c946dfd01443912ee7f42287f393cce1bcac
j4s1x <chessboy14@gmail.com>
2017-07-14 22:10:18 -0600

    modified tests and geckodriver log

--- blogs/tests.py
+++ blogs/tests.py
@@ -2,20 +2,14 @@ from django.test import TestCase
 from .models import Posts
 from selenium import webdriver
 import unittest
+from blogs.views import index
+from django.urls import resolve
 
-class UserFirstVisit(unittest.TestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_user_index_page(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('The Blog Title', self.browser.title)
-        self.fail('Test Complete!')
-
+class HomePageTest(TestCase):
+    '''confirm home page view is correct'''
+    def test_root_url_correct(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
